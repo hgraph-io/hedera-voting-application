03e8f94b3b9a9c14bc2bbf13242d1d17e22b61d3
Patches <hgraphpunks@gmail.com>
2023-06-22 17:26:56 -0600

    Update admin login and dashboard

--- src/components/AdminDashboard.tsx
+++ src/components/AdminDashboard.tsx
@@ -1,20 +1,40 @@
 import React from 'react';
-import styles from './AdminDashboard.module.scss';
+import { Typography, Container, Button, Link } from '@mui/material';
+import { useRouter } from 'next/router';
+import CardComponent from '../components/Card';
+import styles from './DashboardPage.module.scss';
 
-const AdminDashboard: React.FC = () => {
-  const cards = [
-    // Example data for cards
-    { type: 'vote', speaker: 'John Doe', title: 'Card 2', isSelected: false, rating: { voteNum: 30, currentRating: 3 } },
-    // More cards...
-  ];
+const DashboardPage: React.FC = () => {
+  const router = useRouter();
+
+  const handleViewAll = () => {
+    router.push('/admin-results');
+  }
 
   return (
-    <div className={styles.adminDashboard}>
-      {/* {cards.map((card, index) => (
-        <Card key={index} {...card} />
-      ))} */}
-    </div>
+    <Container maxWidth="md" className={styles.dashboardContainer}>
+      <Typography variant="h3">Admin Dashboard</Typography>
+      <Typography>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum viverra sed justo vestibulum commodo. Phasellus id urna mollis, sollicitudin neque eu, dictum purus.
+      </Typography>
+      <Button variant="outlined" onClick={handleViewAll}>
+        View All
+      </Button>
+      
+      <Typography variant="h3">Applications</Typography>
+      <Typography>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum viverra sed justo vestibulum commodo. Phasellus id urna mollis, sollicitudin neque eu, dictum purus.
+      </Typography>
+      
+      <div>
+        <CardComponent title='Application 1' applicationId={1} rating={{voteNum:1,currentRating:5}} speaker='John Doe' isSelected={true} type='view' />
+        <CardComponent title='Application 2' applicationId={2} rating={{voteNum:1,currentRating:5}} speaker='Jane Doe' isSelected={false} type='vote' />
+        <CardComponent title='Application 3' applicationId={3} rating={{voteNum:1,currentRating:5}} speaker='John Doe' isSelected={true} type='approved' />
+        <CardComponent title='Application 4' applicationId={4} rating={{voteNum:1,currentRating:5}} speaker='Jane Doe' isSelected={true} type='denied' />
+        <CardComponent title='Application 5' applicationId={5} rating={{voteNum:1,currentRating:5}} speaker='John Doe' isSelected={true} type='default' />
+      </div>
+    </Container>
   );
 };
 
-export default AdminDashboard;
+export default DashboardPage;
--- src/components/AdminLogin.tsx
+++ src/components/AdminLogin.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
+import { Container, TextField, Button, Typography } from '@mui/material';
 import styles from './AdminLogin.module.scss';
 
 const AdminLogin: React.FC = () => {
   return (
     <div className={styles.adminLogin}>
-      <h1>Admin Login</h1>
-      <form>
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Log In</button>
-      </form>
+      <Container maxWidth="sm">
+        <Typography variant="h4" component="h1" align="center" className={styles.title}>
+          Admin Login
+        </Typography>
+        <form noValidate autoComplete="off">
+          <TextField label="Username" variant="outlined" fullWidth className={styles.inputField} />
+          <TextField label="Password" variant="outlined" type="password" fullWidth className={styles.inputField} />
+          <Button variant="contained" color="primary" type="submit" fullWidth className={styles.submitBtn}>
+            Log In
+          </Button>
+        </form>
+      </Container>
     </div>
   );
 };
