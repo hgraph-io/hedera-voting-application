d5d676657b01ca46638961edbad54b8bd2e91133
Patches <hgraphpunks@gmail.com>
2023-06-23 15:07:26 -0600

    update register and application pages

--- src/components/ApplicationPage.tsx
+++ src/components/ApplicationPage.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import styles from './ApplicationPage.module.scss';
-import { Typography, Button, Container, Table, TableBody, TableCell, TableContainer, TableRow, Paper, TableHead } from '@mui/material';
+import { Typography, Button, Container, Table, TableBody, TableCell, TableContainer, TableRow, Paper, TableHead, Grid } from '@mui/material';
 import StarIcon from '@mui/icons-material/Star';
 import axios from 'axios';
 import { useRouter } from 'next/router';
@@ -27,7 +27,6 @@ const ApplicationPage: React.FC<Props> = ({applicationData}) => {
   const [userVote, setUserVote] = useState<number>(4);
   const userId = 'userId'; // replace with actual logged in user id
 
-  // Simulating an API call
   useEffect(() => {
     const fetchVotes = async () => {
       const response = {data:[]}//await axios.get('https://hgraph.io/backed'); // replace with actual API
@@ -45,69 +44,82 @@ const ApplicationPage: React.FC<Props> = ({applicationData}) => {
     router.back();
   }
 
-
   return (
-    <div className={styles.adminDashboard}>
-      <Button variant="outlined" onClick={goBack} style={{ marginTop: "20px", marginLeft: "20px" }}>Go Back</Button>
-
-      <Typography variant="h2">Application</Typography>
+    <Grid container spacing={3} className={styles.adminDashboard}>
+      <Grid item xs={12}>
+        <Button variant="outlined" onClick={goBack} style={{ marginTop: "20px", marginLeft: "20px" }}>Go Back</Button>
+      </Grid>
+      <Grid item xs={12}>
+        <Typography variant="h2">Application</Typography>
+      </Grid>
       {applicationData.map((data, index) => (
-        <Container key={index} className={styles.applicationContainer}>
-          <Typography variant="body1">Speaker Name</Typography>
-          <Typography variant="h6">{data.name}</Typography>
-          <br/>
-          <Typography variant="body1">Related Topics:</Typography>
-          <ul>
-            {data.topics.map((topic, topicIndex) => (
-              <li key={topicIndex}>{topic}</li>
-            ))}
-          </ul>
-          <br/>
-          <Typography variant="body1">Links:</Typography>
-          <ul>
-            {data.links.map((link, linkIndex) => (
-              <li key={linkIndex}><a href={link}>{link}</a></li>
-            ))}
-          </ul>
-          <br/>
-          <Typography variant="h6">Your Vote: </Typography> 
-          <div className={styles.rating}>
-            {[...Array(5)].map((_, i) => (
-              <StarIcon key={i} className={styles.star} style={{ color: i < userVote ? 'gold' : 'grey' }} />
-            ))}
-          </div>
-          <Typography variant="body1">{userVote}/5</Typography>
-          <br/>
-          <Typography variant="h6">Total Votes:</Typography>
-          <div className={styles.rating}>
-            {[...Array(5)].map((_, i) => (
-              <StarIcon key={i} className={styles.star} style={{ color: i < 4 ? 'gold' : 'grey' }} />
-            ))}
-          </div>
-          <Typography variant="body2">4.21 out of 5</Typography>
-          <br/>
-          <Typography variant="body1">Below you can see all of the current votes on this application:</Typography>
-          <TableContainer component={Paper} className={styles.voteTable}>
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell>Account ID</TableCell>
-                  <TableCell>Voted (1-5)</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {votes.map((vote) => (
-                  <TableRow key={vote.accountId}>
-                    <TableCell>{vote.accountId}</TableCell>
-                    <TableCell>{vote.vote}</TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </Container>
+        <Grid item xs={12} key={index}>
+          <Container className={styles.applicationContainer}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <Typography variant="body1">Speaker Name</Typography>
+                <Typography variant="h6">{data.name}</Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography variant="body1">Related Topics:</Typography>
+                <ul>
+                  {data.topics.map((topic, topicIndex) => (
+                    <li key={topicIndex}>{topic}</li>
+                  ))}
+                </ul>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography variant="body1">Links:</Typography>
+                <ul>
+                  {data.links.map((link, linkIndex) => (
+                    <li key={linkIndex}><a href={link}>{link}</a></li>
+                  ))}
+                </ul>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography variant="h6">Your Vote: </Typography> 
+                <div className={styles.rating}>
+                  {[...Array(5)].map((_, i) => (
+                    <StarIcon key={i} className={styles.star} style={{ color: i < userVote ? 'gold' : 'grey' }} />
+                  ))}
+                </div>
+                <Typography variant="body1">{userVote}/5</Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography variant="h6">Total Votes:</Typography>
+                <div className={styles.rating}>
+                  {[...Array(5)].map((_, i) => (
+                    <StarIcon key={i} className={styles.star} style={{ color: i < 4 ? 'gold' : 'grey' }} />
+                  ))}
+                </div>
+                <Typography variant="body2">4.21 out of 5</Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography variant="body1">Below you can see all of the current votes on this application:</Typography>
+                <TableContainer component={Paper} className={styles.voteTable}>
+                  <Table>
+                    <TableHead>
+                      <TableRow>
+                        <TableCell>Account ID</TableCell>
+                        <TableCell>Voted (1-5)</TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {votes.map((vote) => (
+                        <TableRow key={vote.accountId}>
+                          <TableCell>{vote.accountId}</TableCell>
+                          <TableCell>{vote.vote}</TableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+              </Grid>
+            </Grid>
+          </Container>
+        </Grid>
       ))}
-    </div>
+    </Grid>
   );
 };
 
--- src/components/RegisterPage.tsx
+++ src/components/RegisterPage.tsx
@@ -1,14 +1,54 @@
 import React, { useState } from 'react';
-import { TextField, Button, Container, Typography } from '@mui/material';
+import { TextField, Button, Container, Typography, Select, MenuItem, FormControl, InputLabel } from '@mui/material';
 import axios from 'axios';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/router' // next/router instead of next/navigation
+import {useUser} from '../pages/_app'
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 import styles from './RegisterPage.module.scss';
 
+import type { Database } from '@/lib/database.types'
 
 const RegisterPage: React.FC = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
+  const [method, setMethod] = useState('Email');
+
+  const supabase = createClientComponentClient<Database>()
+
+  const handleSignUp = async () => {
+    await supabase.auth.signUp({
+      email,
+      password,
+      options: {
+        emailRedirectTo: `${location.origin}/dashboard`,
+      },
+    })
+    router.refresh()
+  }
+
+  const handleSignIn = async () => {
+    await supabase.auth.signInWithPassword({
+      email,
+      password,
+    })
+    router.refresh()
+  }
+
+  const handleSignOut = async () => {
+    await supabase.auth.signOut()
+    router.refresh()
+  }
+
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post('/api/login', { email, password });
+      alert(response.data.message);
+      router.push('/dashboard');
+    } catch (error) {
+      alert(error.response?.data?.error || 'Login failed');
+    }
+  };
 
   const router = useRouter(); // get the router object
 
@@ -24,41 +64,64 @@ const RegisterPage: React.FC = () => {
       alert(error.response?.data?.error || 'Registration failed');
     }
   };
-
   return (
-    <Container className={styles.registerPageContainer} maxWidth="xs">
+    <Container className={styles.loginPageContainer} maxWidth="xs">
       <Typography component="h1" variant="h5">
         Register
       </Typography>
-      <form noValidate autoComplete="off">
-        <TextField
-          margin="normal"
-          required
-          fullWidth
-          label="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <TextField
-          margin="normal"
-          required
-          fullWidth
-          label="Password"
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <TextField
-          margin="normal"
-          required
-          fullWidth
-          label="Confirm Password"
-          type="password"
-          value={confirmPassword}
-          onChange={(e) => setConfirmPassword(e.target.value)}
-        />
-        <Button variant="contained" color="primary" fullWidth onClick={handleRegister}>
-          Register
+      <form className={styles.formContent} noValidate autoComplete="off">
+        <FormControl fullWidth>
+          <InputLabel id="method-select-label">Method</InputLabel>
+          <Select
+            labelId="method-select-label"
+            id="method-select"
+            value={method}
+            label="Method"
+            onChange={(e) => setMethod(e.target.value)}
+          >
+            <MenuItem value={'Email'}>Email</MenuItem>
+            <MenuItem value={'Hashpack'}>Hashpack</MenuItem>
+          </Select>
+        </FormControl>
+        {method === 'Email' ? (
+          <>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              label="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              label="Password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              label="Confirm Password"
+              type="password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+            <Button variant="contained" color="primary" fullWidth onClick={handleSignUp}>
+              Login
+            </Button>
+          </>
+        ) : (
+          <Button variant="contained" color="primary" fullWidth onClick={handleSignIn}>
+            Sign Up with Hashpack
+          </Button>
+        )}
+        <Button variant="outlined" fullWidth onClick={() => router.push('/admin-login')}>
+          Admin Login
         </Button>
       </form>
     </Container>
