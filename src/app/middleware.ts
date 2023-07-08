b4a9180ea83f00267ff442f79f51fad3bccd4204
splash <splash@hgraph.io>
2023-07-08 10:52:52 -0600

    user submit structure

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -7,6 +7,31 @@ import type { Database } from '@/types';
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
   const supabase = createMiddlewareClient<Database>({ req, res });
-  await supabase.auth.getSession();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  // if user is signed in and the current path is / redirect the user to /submit
+  if (user && req.nextUrl.pathname === '/')
+    return NextResponse.redirect(new URL('/submit', req.url));
+  // if user is not signed in and the current path is not / or /login redirect the user to /
+  else if (!user && !['/', '/login'].includes(req.nextUrl.pathname)) {
+    return NextResponse.redirect(new URL('/', req.url));
+  }
+
   return res;
 }
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - vote (vote routes to be handled by hedera wallet)
+     * - public (public files)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    '/((?!vote|public|_next/static|_next/image|favicon.ico).*)',
+  ],
+};
