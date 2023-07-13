48d1b168115ce7ad16a20c43abe055b394f07b3c
Patches <hgraphpunks@gmail.com>
2023-07-13 14:08:35 -0600

    Update create theme styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,64 +12,103 @@ import { SessionContextProvider, Session } from '@supabase/auth-helpers-react';
 import { SnackbarProvider } from '../contexts/SnackbarContext';
 import { UserProvider, useUser } from '../contexts/UserContext';
 
+const colors = {
+  blue: '#4263eb',
+  purple: '#7048e8',
+  darkPurple: '#6541d2',
+  green: '#51cf66',
+  darkGreen: '#21a056',
+  yellow: '#eada0b',
+  lime: '#b7f133',
+  black: '#212429',
+  white: '#f9f9f9',
+  whiteText: '#FFFFFF',
+  grey1: '#dde2e5',
+  grey2: '#acb5bd',
+  grey3: '#495057',
+};
+
 //@ts-ignoe
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#7048E8',
-      contrastText: '#ffcc00',
+      main: colors.blue,
+      contrastText: colors.yellow,
     },
     secondary: {
-      light: '#51CF66',
-      main: '#7048E8',
-      contrastText: '#ffcc00',
+      light: colors.green,
+      main: colors.purple,
+      contrastText: colors.yellow,
     },
     text: {
-      primary: '#212429',
-      secondary: '#495057',
+      primary: colors.black,
+      secondary: colors.grey3,
     },
   },
   typography: {
     fontFamily: 'Styrene A Web, Arial',
   },
   components: {
+    MuiAppBar: {
+      styleOverrides: {
+        root: {
+          backgroundColor: colors.white, // Set AppBar background to white
+          color: colors.black, // Set text color to black
+        },
+      },
+    },
     MuiButton: {
+      variants: [
+        {
+          props: { variant: 'gradient' },
+          style: {
+            color: colors.white,
+            background: colors.darkGreen,
+            borderRadius: '50px',
+            border: 'none',
+            textTransform: 'capitalize',
+            '&:hover': {
+              background: colors.green,
+            },
+          },
+        },
+      ],
       styleOverrides: {
         text: {
           height: '32px',
-          minWidth: '200px',
           borderRadius: '50px',
-          border:'none',
-          color: '#212429',
-          background: '#F9F9F9',
+          minWidth: '100px',
+          border: 'none',
+          color: colors.black,
+          background: colors.white,
           textTransform: 'capitalize',
           '&:hover': {
-            backgroundColor: 'linear-gradient(45deg, #21A056, #3EC878)',
+            backgroundColor: colors.purple,
           },
         },
         contained: {
           height: '32px',
           minWidth: '100px',
           borderRadius: '50px',
-          border:'none',
-          color: '#F9F9F9',
-          background: '#8259EF',
+          border: 'none',
+          color: colors.white,
+          background: colors.darkPurple,
           textTransform: 'capitalize',
           '&:hover': {
-            backgroundColor: 'linear-gradient(45deg, #21A056, #3EC878)',
+            backgroundColor: colors.purple,
           },
         },
         outlined: {
           height: '32px',
-          minWidth: '200px',
-          color: '#212429',
-          border: '1px solid #212429',
+          color: colors.black,
+          minWidth: '100px',
+          border: `1px solid ${colors.black}`,
           borderRadius: '50px',
           textTransform: 'capitalize',
-          background: '#f9f9f9',
           '&:hover': {
-            color: '#FFFFFF',
-            backgroundColor: '#7048E8',
+            color: colors.whiteText,
+            backgroundColor: colors.purple,
+            border: `none`,
           },
         },
       },
@@ -78,13 +117,6 @@ const theme = createTheme({
       styleOverrides: {
         root: {
           margin: '20px 0px 0px',
-        },
-      },
-    },
-    MuiAppBar: {
-      styleOverrides: {
-        root: {
-          backgroundColor: '#f9f9f9',
         },
       },
     },
