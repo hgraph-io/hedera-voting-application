ad032d644f2777ff69ea7068eb1ba17ec72bd816
splash <splash@hgraph.io>
2023-07-15 08:57:47 -0600

    remove extraneous

--- src/app/admin/vote/utils.ts
+++
@@ -1,8 +0,0 @@
-export const hex2a = (hexx: string): string => {
-  let hex = hexx.toString().split('\\x')[1]; //force conversion
-  let str = '';
-  for (let i = 0; i < hex.length; i += 2) {
-    str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
-  }
-  return str;
-};
