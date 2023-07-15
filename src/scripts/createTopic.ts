add15b004cae94fc682fbbcf1c35ede8c8a38750
splash <splash@hgraph.io>
2023-07-15 16:01:58 -0600

    script to create a new topic

---
+++ src/scripts/createTopic.ts
@@ -0,0 +1,32 @@
+require('dotenv').config({ path: '.env.local' });
+
+import { AccountId, PrivateKey, Client, TopicCreateTransaction, TopicId } from '@hashgraph/sdk';
+
+if (
+  !process.env.HEDERA_ACCOUNT_PRIVATE_KEY ||
+  !process.env.HEDERA_NETWORK ||
+  !process.env.HEDERA_ACCOUNT_ID
+)
+  throw new Error('One of the required env vars has not been set');
+
+if (process.env.HEDERA_TOPIC_ID) throw new Error('Topic already created');
+
+const network = process.env.HEDERA_NETWORK;
+const accountPrivateKey = PrivateKey.fromString(process.env.HEDERA_ACCOUNT_PRIVATE_KEY);
+const accountId = AccountId.fromString(process.env.HEDERA_ACCOUNT_ID);
+
+const client = Client[network === 'mainnet' ? 'forMainnet' : 'forTestnet']().setOperator(
+  accountId,
+  accountPrivateKey
+);
+
+async function main() {
+  const transactionId = await new TopicCreateTransaction().execute(client);
+  const receipt = await transactionId.getReceipt(client);
+  const topicId = receipt.topicId as TopicId;
+
+  console.log(`Created new topic ${topicId}`);
+  process.exit();
+}
+
+main();
